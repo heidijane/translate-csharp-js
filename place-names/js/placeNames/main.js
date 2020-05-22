a46b0fd8a18631e58de78cd665193eacb539bc67
Heidi Sprouse <hsprouse91@gmail.com>
2020-05-22 14:24:03 -0500

    translated C# into JS for place-names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,31 @@
 function main() {
-  // Put your code here
+
+  const names = [
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China"
+  ];
+
+  console.log("All Place Names");
+
+  for (name of names) {
+    console.log(name);
+  }
+
+  console.log("");
+
+  const theNames = names.filter(name => name.startsWith("The"))
+
+  console.log("'The' Place Names");
+  for (name of theNames) {
+    console.log(name);
+  }
+
 
 }
 
