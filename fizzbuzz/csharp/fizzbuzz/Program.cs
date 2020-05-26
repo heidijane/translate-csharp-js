99010f6e959c1830d0befee2f0e83bfff1ac6c5c
Heidi Sprouse <hsprouse91@gmail.com>
2020-05-26 11:46:29 -0500

    Translated fizzbuzz from JS to C#

---
+++ fizzbuzz/csharp/fizzbuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++)
+            {
+                if (num % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
